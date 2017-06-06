2378564096cdcbdf1389c4ae8eeddb1688d6b8f4
Daniel Testa <daniel.testa@autodesk.com>
2017-06-05 17:48:51 -0700

    completed lesson 2

---
+++ src/csv_zen/core.clj
@@ -0,0 +1,20 @@
+(ns csv-zen.core
+  (:require [ring.adapter.jetty :as jetty]
+            [hugsql.core :as hugsql]))
+
+(def db (System/getenv "DATABASE_URL"))
+(def db "jdbc:postgresql://localhost/csvzen")
+
+(hugsql/def-db-fns "csv_zen/sql/database.sql")
+
+(defn create-endpoint [db]
+  (:id (create-endpoint* db)))
+
+(defn app [req]
+  {:headers {}
+   :status 200
+   :body "Hello, World!"})
+
+(defn -main []
+  (jetty/run-jetty app
+                   {:port 8080}))
---
+++ test/csv_zen/core_test.clj
@@ -0,0 +1,7 @@
+(ns csv-zen.core-test
+  (:require [clojure.test :refer :all]
+            [csv-zen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
