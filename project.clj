23ab7848cef476854baa8120d084b2bae7f00df5
Daniel Testa <daniel.testa@autodesk.com>
2017-08-29 21:17:05 -0700

    serve static files from yada

--- project.clj
+++ project.clj
@@ -7,5 +7,15 @@
                  [ring "1.6.1"]
                  [com.layerware/hugsql "0.4.7"]
                  [org.postgresql/postgresql "9.4.1207"]
-                 [org.clojure/data.csv "0.1.4"]]
-  :main csv-zen.core)
+                 [org.clojure/data.csv "0.1.4"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
+                 [hiccup "1.0.5"]
+                 [bidi "2.1.2"]
+                 [aleph "0.4.3"]
+                 [yada "1.2.6"]]
+  :main csv-zen.core
+
+  :profiles
+  {:dev {:source-paths ["dev-src"]
+         :main csv-zen.dev
+         :dependencies [[prone "1.1.4"]]}})
--- src/csv_zen/core.clj
+++ src/csv_zen/core.clj
@@ -2,7 +2,25 @@
   (:require [ring.adapter.jetty :as jetty]
             [hugsql.core :as hugsql]
             [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [ring.middleware.params :as params]
+            [ring.middleware.resource :as resource]
+            [ring.middleware.content-type :as content-type]
+            [ring.middleware.not-modified :as not-modified]
+            [ring.middleware.multipart-params :as multipart]
+            [clojure.core.match :refer [match]]
+            [clojure.string :as str]
+            [clojure.pprint :as pprint]
+            [ring.handler.dump :as dump]
+            [hiccup.core :refer [html h]]
+            [hiccup.page :as page]
+            [bidi.bidi :as bidi]
+            [bidi.vhosts :refer [vhosts-model]]
+            [yada.yada :as yada]
+            [yada.resources.classpath-resource :refer [new-classpath-resource]]))
+
+(def scheme "http")
+(def host "localhost:8080")
 
 (def db (System/getenv "DATABASE_URL"))
 (def db "jdbc:postgresql://localhost/csvzen")
@@ -49,8 +67,8 @@
     (read-and-insert db upload-id rdr)
     upload-id))
 
-(defn do-insert-from-file [db endpoint-id filename]
-  (with-open [rdr (io/reader (io/file filename))]
+(defn do-insert-from-file [db endpoint-id file]
+  (with-open [rdr (io/reader file)]
     (do-upload db endpoint-id rdr)))
 
 (defn keys-for-upload [db upload-id]
@@ -67,12 +85,103 @@
         {:row-id row-id}
         cells))))
 
-(defn app [req]
-  {:headers {}
-   :status 200
-   :body "Hello, World!"})
+(defn segments-from-path [uri]
+  (if (= "/" uri)
+    []
+    (-> uri
+           (.substring 1)
+           (str/split #"/"))))
+
+(defn create-endpoint-request [ctx]
+  (let [endpoint-id (create-endpoint db)]
+    (java.net.URI. (yada/url-for ctx :endpoint {:route-params {:id endpoint-id}}))))
+
+(defn upload-request [ctx]
+  (let [req (:request ctx)
+        endpoint-id (get-in req [:route-params :id])
+        endpoint-id (java.util.UUID/fromString endpoint-id)
+        multipart (get-in ctx [:parameters :form :file])
+        upload-id (do-upload db endpoint-id (java.io.StringReader. multipart))]
+    (java.net.URI. (yada/url-for ctx :endpoint-upload {:route-params {:id endpoint-id :upload-id upload-id}}))))
+
+(defn dashboard-page []
+  (page/html5
+   {:lang :en}
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:meta {:http-equiv "X-UA-Compatible"
+            :content "IE=edge"}]
+    [:meta {:name "viewport"
+            :content "width=device-width, initial-scale=1"}]
+    ;; the above 3 meta tags must come first
+    [:link {:rel "stylesheet"
+            :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+            :integrity "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+            :crossorigin "anonymous"}]
+    [:link {:rel "stylesheet"
+            :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+            :integrity "sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+            :crossorigin "anonymous"}]
+
+    [:title "Dashboard"]]
+
+   [:body
+    [:div.container
+     [:h1  "Dashboard"]
+
+     [:form
+      [:button.btn.btn-default "Click me!"]]
+     ]
+    [:script {:src "https://code.jquery.com/jquery-3.2.1.min.js"}]
+
+    [:script {:src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+              :integrity "sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+              :crossorigin "anonymous"}]
+
+    ]))
+
+(def routes
+  (vhosts-model
+    [[(str scheme "://" host)]
+     ["" [["/dump" dump/handle-dump]
+          ["/dashboard" (yada/resource
+                         {:id :dashboard
+                          :produces {:media-type "text/html"
+                                     :language "en"}
+                          :response (fn [ctx]
+                                      (dashboard-page))})]
+          ["/endpoints" (yada/resource
+                         {:id :endpoints
+                          :produces {:media-type "application/json"}
+                          :methods {:post
+                                    {:response
+                                     (fn [ctx]
+                                       (create-endpoint-request ctx))}}})]
+          [["/endpoint/" :id]
+           {"" (yada/resource
+                {:id :endpoint
+                 :produces {:media-type "application/json"}
+                 :methods {:post
+                           {:consumes "multipart/form-data"
+                            :parameters {:form {:file String}}
+                            :response upload-request}}})
+            ["/upload/" :upload-id] (yada/resource
+                                     {:id :endpoint-upload
+                                      :response nil})}]
+          ["" (new-classpath-resource "homepage"
+                                      {:index-files ["index.html"]})]]]]))
+
+(defonce server (atom nil))
 
 (defn -main []
   (init-db)
-  (jetty/run-jetty app
-                   {:port 8080}))
+  (let [listener  (yada/listener routes
+                                 {:port 8080})]
+    (reset! server listener)))
+
+(defn reset []
+  (when (fn? (:close @server))
+    ((:close @server)))
+  (-main))
+
+(reset)
